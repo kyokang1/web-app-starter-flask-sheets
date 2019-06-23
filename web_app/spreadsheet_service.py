d5285b8572981aa73fcc3f5da430692a370746ea
kyokang1 <kyoungmokang@gmail.com>
2019-06-22 22:23:53 -0400

    update

--- web_app/spreadsheet_service.py
+++ web_app/spreadsheet_service.py
@@ -10,6 +10,7 @@ import os
 
 from dotenv import load_dotenv
 import gspread
+
 from gspread.exceptions import SpreadsheetNotFound
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -18,16 +19,20 @@ load_dotenv()
 DOCUMENT_KEY = os.environ.get("GOOGLE_SHEET_ID", "OOPS Please get the spreadsheet identifier from its URL")
 SHEET_NAME = "Products"
 
-#CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "auth", "spreadsheet_credentials.json")
+CREDENTIALS_FILEPATH = os.path.join(os.path.dirname(__file__), ".", "auth", "google_api_credentials.json")
 GOOGLE_API_CREDENTIALS = os.environ.get("GOOGLE_API_CREDENTIALS")
+
 AUTH_SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets", #> Allows read/write access to the user's sheets and their properties.
-    "https://www.googleapis.com/auth/drive.file" #> Per-file access to files created or opened by the app.
+    "https://www.googleapis.com/auth/drive",
+    "https://spreadsheets.google.com/feeds"
 ]
+    #"https://www.googleapis.com/auth/spreadsheets", #> Allows read/write access to the user's sheets and their properties.
+    #"https://www.googleapis.com/auth/drive.file" #> Per-file access to files created or opened by the app.
+
 
 def get_products():
-    #credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
-    credentials = ServiceAccountCredentials._from_parsed_json_keyfile(json.loads(GOOGLE_API_CREDENTIALS), AUTH_SCOPE)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILEPATH, AUTH_SCOPE)
+    #credentials = ServiceAccountCredentials._from_parsed_json_keyfile(json.loads(GOOGLE_API_CREDENTIALS), AUTH_SCOPE)
     client = gspread.authorize(credentials) #> <class 'gspread.client.Client'>
     print("GETTING PRODUCTS FROM THE SPREADSHEET...")
     doc = client.open_by_key(DOCUMENT_KEY) #> <class 'gspread.models.Spreadsheet'>
@@ -40,7 +45,6 @@ def create_product(product_attributes, sheet=None, products=None):
     if not (sheet and products):
         print("OH, PREFER TO PASS PREVIOUSLY-OBTAINED SHEET AND PRODUCTS, FOR FASTER PERFORMANCE!")
         sheet, products = get_products()
-
     print(f"DETECTED {len(products)} EXISTING PRODUCTS")
     print("NEW PRODUCT ATTRIBUTES:", product_attributes)
     next_id = len(products) + 1 # number of records, plus one. TODO: max of current ids, plus one
@@ -53,7 +57,6 @@ def create_product(product_attributes, sheet=None, products=None):
     }
     next_row = list(product.values()) #> [13, 'Product CLI', 'snacks', 4.99, '2019-01-01']
     next_row_number = len(products) + 2 # number of records, plus a header row, plus one
-
     response = sheet.insert_row(next_row, next_row_number)
     return response
 
